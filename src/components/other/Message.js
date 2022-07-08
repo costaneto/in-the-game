54dc6aaffc08e4eca13b6542ee97256ea7fb3018
CostaNeto <costaneto50@outlook.pt>
2022-07-08 15:31:25 +0100

    Finished tutor actions

--- src/components/other/Message.js
+++ src/components/other/Message.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Message = ({ isAction, onCloseAction }) => {
+	const submit = (e) => {
+		onCloseAction(e, "message");
+	};
+
 	return (
 		<div className={`action${isAction ? " show" : ""}`} id="action_message">
-			<form
-				className="message-form"
-				onSubmit={(e) => onCloseAction(e, "message")}
-			>
-				<p>What's your name?</p>
-				<input type="text" placeholder="name" />
-				<p>Type your message</p>
-				<textarea placeholder="message" />
+			<form className="message-form" onSubmit={(e) => submit(e)}>
+				<p>Your name</p>
+				<div className="message-name">
+					<input type="text" placeholder="name" />
+				</div>
+				<p>Say something</p>
+				<div className="message-text">
+					<textarea placeholder="message" />
+				</div>
 				<button type="submit">Send</button>
 			</form>
 		</div>
